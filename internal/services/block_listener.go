37987bb004fd7130a75d6d8b5e8b3b38fa90cbbc
Dylan Moreland <79415431+elffjs@users.noreply.github.com>
2024-02-06 16:37:31 -0500

    Remove unused function. Use the metrics in it (#18)
    * Remove unused function. Use the metrics in it
    
    * Hash

--- internal/services/block_listener.go
+++ internal/services/block_listener.go
@@ -270,58 +270,38 @@ func (bl *BlockListener) GetBlockHead(blockNum *big.Int) (*types.Header, error) 
 	return head, err
 }
 
-func (bl *BlockListener) GetFilteredBlockLogs(bHash common.Hash, contracts []common.Address) ([]types.Log, error) {
-	timer := prometheus.NewTimer(metrics.FilteredLogsResponseTime)
-	fil := ethereum.FilterQuery{
-		BlockHash: &bHash,
-		Addresses: bl.contracts,
-	}
-
-	var logs []types.Log
-	err := retry(func() error {
-		var err error
-		logs, err = bl.client.FilterLogs(context.Background(), fil)
-		if err != nil {
-			metrics.CountRetries.With(prometheus.Labels{"type": "FilterLogs"}).Inc()
-		}
-		return err
-	}, MaxRetries, RetryDuration)
-	if err != nil {
-		metrics.FailedFilteredLogsFetch.Inc()
-		return []types.Log{}, nil
-	}
-	timer.ObserveDuration()
-	metrics.SuccessfulFilteredLogsFetch.Inc()
-	metrics.CountRetries.With(prometheus.Labels{"type": "FilterLogs"}).Set(0)
-	return logs, err
-}
-
 func (bl *BlockListener) ProcessBlock(ctx context.Context, blockNum *big.Int) error {
 	logger := bl.logger.With().Int64("chainId", bl.chainID).Int64("blockNumber", blockNum.Int64()).Logger()
 
-	logger.Info().Msg("Processing block.")
 	head, err := bl.GetBlockHead(blockNum)
 	if err != nil {
-		return err
+		return fmt.Errorf("error getting block %d header: %w", blockNum, err)
 	}
+
+	logger.Info().Str("hash", head.Hash().Hex()).Msg("Processing block.")
 
 	tm := time.Unix(int64(head.Time), 0)
 	var logs []types.Log
 	err = retry(func() error {
 		var err error
+		timer := prometheus.NewTimer(metrics.FilteredLogsResponseTime)
 		logs, err = bl.client.FilterLogs(ctx, ethereum.FilterQuery{
 			FromBlock: blockNum,
 			ToBlock:   blockNum,
 			Addresses: bl.contracts,
 		})
 		if err != nil {
+			metrics.FailedFilteredLogsFetch.Inc()
 			metrics.CountRetries.With(prometheus.Labels{"type": "FilterLogs"}).Inc()
+		} else {
+			timer.ObserveDuration()
 		}
 		return err
 	}, MaxRetries, RetryDuration)
 	if err != nil {
 		return fmt.Errorf("failed retrieving logs: %w", err)
 	}
+	metrics.SuccessfulFilteredLogsFetch.Inc()
 	metrics.CountRetries.With(prometheus.Labels{"type": "FilterLogs"}).Set(0)
 
 	for _, vLog := range logs {
